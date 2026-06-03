607cbd3fbd3ea1e4bf3541b0bbd8b58fed99044b
Abhi Maradiya <abhimaradiya63@gmail.com>
2026-06-03 14:50:40 +0530

    Added JOI for Server Side Validation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate')
 const wrapAsync=require('./utils/wrapAsync.js')
 const ExpressError=require('./utils/ExpressError.js')
+const {listingSchema}=require("./schema.js")
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"))
@@ -28,8 +29,6 @@ main()
     .catch((err) => {
         console.log('Connecttion Errorr', err);
     })
-
-
 async function main() {
     await mongoose.connect(MONGODB_URL)
 }
@@ -55,10 +54,12 @@ app.get('/listing/:id', wrapAsync(async (req, res) => {
 
 //New Listing is added 
 app.post('/listing', wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-        throw new ExpressError(400,'Send Valide Data for listing')
-    }
-        const newListing =new Listing(req.body.listing)
+        let result=listingSchema.validate(req.body);
+        console.log(result);
+        if(result.error){
+            throw new ExpressError(400,result.error)
+        }
+        const newListing =new Listing(req.body.listing);
         await newListing.save();
         res.redirect('/listing')
 }));
