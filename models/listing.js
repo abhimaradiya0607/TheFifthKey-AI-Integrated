e89dd3d3083ffc0d766f44386aa74d2bd8b955c1
Abhi Maradiya <abhimaradiya63@gmail.com>
2026-06-06 12:24:14 +0530

    fixed: Listing and Review model linked

--- models/listing.js
+++ models/listing.js
@@ -1,45 +1,51 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+const Review = require("./reviews.js")
 
 
-let listingSchema=mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+let listingSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String
+    description: {
+        type: String
     },
-    image:{
-        filename:{
-            type:String,
-            default:"Listing Image",
+    image: {
+        filename: {
+            type: String,
+            default: "Listing Image",
         },
-        url:{
-            type:String,
-            set: (v) =>v === "" ? "https://images.unsplash.com/photo-1552566626-52f8b828add9?w=800" : v,
-            default:"https://images.unsplash.com/photo-1552566626-52f8b828add9?w=800",
+        url: {
+            type: String,
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1552566626-52f8b828add9?w=800" : v,
+            default: "https://images.unsplash.com/photo-1552566626-52f8b828add9?w=800",
         }
     },
-    price:{
-        type:Number,
-        required:true,
-        min:0
+    price: {
+        type: Number,
+        required: true,
+        min: 0
     },
-    location:{
-        type:String
+    location: {
+        type: String
     },
-    country:{
-        type:String
+    country: {
+        type: String
     },
-    reviews:[
+    reviews: [
         {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Reviews",
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Reviews",
         }
     ]
 
 });
 
-const Listing=mongoose.model('Listing',listingSchema);
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } })
+    }
+});
 
-module.exports=Listing;
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
